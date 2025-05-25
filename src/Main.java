311e7a38e4ec77321e96c86e364f72f0126f119e
Taiinyy <manuvb.349@gmail.com>
2025-05-25 18:15:34 -0500

    Agregar clase Main para iniciar la aplicación y mostrar la vista de inicio de sesión; actualizar .gitignore para evitar incluir archivo de configuración sensible

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import view.LoginView;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Set system look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            LoginView loginView = new LoginView();
+            loginView.setVisible(true);
+        });
+    }
+} 
